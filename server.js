39a0eb15fc15f6e2884b182cd18b74fc2494bb13
Su Thaw <suthaw524@gmail.com>
2015-03-06 11:41:47 +0630

    Modified

--- server.js
+++ server.js
@@ -9,8 +9,5 @@ app.use(express.static(__dirname + '/public'));
 app.get('*',function(req,res){
 	res.render('index');
 });
-<<<<<<< HEAD
+
 app.listen(process.env.PORT || 3000 );
-=======
-app.listen(3000 || process.env.PORT);
->>>>>>> 21f9141f48fad2e6feb2dc38082c28993b50e49f
